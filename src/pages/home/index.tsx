0e71107d93f9feb47b9a666df4ced0a68b0ee076
mayconbalves <mayconmustaine@hotmail.com>
2022-03-08 22:35:56 -0300

    feat: create validation password

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -9,7 +9,6 @@ const Home = () => {
 
   const inputChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target
-    console.log(value, 'value')
     setValues({
       ...values,
       [name]: value
@@ -17,6 +16,7 @@ const Home = () => {
   }
 
   const handleSubmit = async () => {
+    passwordValidated(values.password)
     const data = await fetch(
       'https://61e036950f3bdb0017934eb0.mockapi.io/api/valid-passwords/results',
       {
@@ -30,8 +30,65 @@ const Home = () => {
       : console.log('error ao cadastrar')
   }
 
-  const onlyNumber = (number: string) => {
-    return number.replace(/\D/g, '').replace(/(\d{6})\d+?$/, '$1')
+  const onlyNumber = (password: string) => {
+    return password.replace(/\D/g, '').replace(/(\d{6})\d+?$/, '$1')
+  }
+
+  const adjacentDigits = (numbers: any): boolean => {
+    const numberArr = [...numbers]
+    numberArr.filter((number, index) => {
+      if (number === numberArr[index + 1]) {
+        return true
+      }
+    })
+
+    return false
+  }
+
+  const growingNumbers = (numbers: any): boolean => {
+    const numberArr = [...numbers]
+    numberArr.every((number, index) => {
+      if (number <= numberArr[index + 1]) {
+        return true
+      }
+    })
+
+    return false
+  }
+
+  const equalNumbers = (password: string): boolean => {
+    if (
+      password === '222222' ||
+      password === '333333' ||
+      password === '444444' ||
+      password === '555555' ||
+      password === '666666' ||
+      password === '777777' ||
+      password === '888888' ||
+      password === '999999'
+    ) {
+      return true
+    }
+
+    return false
+  }
+
+  const passwordValidated = (password: any) => {
+    const passwordArr = [...password].join('')
+    const minRange = 184759
+    const maxRenge = 856920
+
+    if (Number(passwordArr) > minRange && Number(passwordArr) < maxRenge) {
+      if (equalNumbers(passwordArr)) {
+        return setValidate(true)
+      }
+
+      if (adjacentDigits(passwordArr) && growingNumbers(passwordArr)) {
+        return setValidate(true)
+      } else {
+        return setValidate(false)
+      }
+    }
   }
 
   return (
